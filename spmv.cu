f85fe61bfe0b10c2b8efb0dec6ac6f27aa16bb76
Balvansh Heerekar <balvansh@fermi.rlogin>
2020-05-13 09:28:39 -0400

    fixed issues

--- spmv.cu
+++ spmv.cu
@@ -1,10 +1,22 @@
-#include <omp.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <float.h>
 #include <math.h>
+#include <assert.h>
+#include <time.h>
+#include <sys/time.h>
+
+inline cudaError_t _checkError(cudaError_t result)
+{
+  if (result != cudaSuccess) {
+    fprintf(stderr, "CUDA Runtime Error: %s\n", 
+            cudaGetErrorString(result));
+    assert(result == cudaSuccess);
+  }
+  return result;
+}
 
 void read_matrix(int **r_ptr, int** c_ind,float** v, char*fname,int* r_count,int* v_count){	
 	
@@ -84,6 +96,30 @@ void read_matrix(int **r_ptr, int** c_ind,float** v, char*fname,int* r_count,int
     	*v = values;
 }
 
+void cpu_multiply(const int num_rows,const int *ptr, const int *indices, const float *data, const float *x, float* y){
+	float dot;
+	int row_start, row_end;
+	for (int i=0; i<num_rows; i++){
+		dot = 0;
+                /*printf("CPU Matrix\n");
+                for(int l = 0; l<num_rows;l++){
+                        if(l+1 <= num_rows){
+                                for(int k = ptr[l]; k < ptr[l+1];k++){
+                                        printf("%d %d %.10f\n",l+1,indices[k]+1,data[k]);
+                                }
+                        }
+                }*/
+                row_start = ptr[i];
+                row_end = ptr[i + 1];
+                //printf("row_start: %d\t row end: %d\n",row_start, row_end);
+                for(int j = row_start; j < row_end; j++){
+                        //printf("data[%d] = %10f * indices[%d] = %d x[indices[%d]]= %10f\n", i, data[i], i, indices[i]+1, i,  x[indices[i]]);
+                        dot+= data[j] * x[indices[j]];
+                }
+		y[i] += dot;
+	}
+}
+
 __global__ void vector_multiply_unopti(const int num_rows,const int *ptr,const int *indices,const float *data,const float *x, float* y){
 	int row = blockIdx.x * blockDim.x + threadIdx.x;
 	/*printf("Kernel Matrix\n");
@@ -121,12 +157,14 @@ __global__ void vector_multiply_unopti(const int num_rows,const int *ptr,const i
 }
 
 __global__ void vector_multiply_unroll(const int num_rows,const int *ptr,const int *indices,const float *data,const float *x, float* y){
-        int row = blockIdx.x * blockDim.x + threadIdx.x;
+        int row;// = blockIdx.x * blockDim.x + threadIdx.x;
         //printf("In Kernel:\n",row);
         int i;
         int row_start, row_end;
         float dot;
-        if(row < num_rows){
+	#pragma unroll
+        for (row = blockIdx.x * blockDim.x + threadIdx.x; row < num_rows; row += blockDim.x * gridDim.x)
+        {
                 dot = 0;
                 /*printf("Kernel Matrix\n");
                 for(int l = 0; l<num_rows;l++){
@@ -143,7 +181,7 @@ __global__ void vector_multiply_unroll(const int num_rows,const int *ptr,const i
                         dot+= data[i] * x[indices[i]];
                 }
         }
-        y[row] += dot;
+
 }
 
 //TODO: Fix this implementation
@@ -159,9 +197,10 @@ __global__ void vector_multiply_shared(const int num_rows,const int *ptr,const i
         //printf("No error:\n",row);
         int i;
         int row_start, row_end;
-        float dot;
+        //float dot;
         if(row < num_rows){
-                dot = 0;
+                //dot = 0;
+		s[row] = 0.0;
                 /*printf("Kernel Matrix\n");
                 for(int l = 0; l<num_rows;l++){
                         if(l+1 <= num_rows){
@@ -174,10 +213,10 @@ __global__ void vector_multiply_shared(const int num_rows,const int *ptr,const i
                 row_end = ptr[row + 1];
                 #pragma unroll
                 for(i = row_start; i < row_end; i++){
-                        dot+= data[i] * x[indices[i]];
+                        s[row]+= data[i] * x[indices[i]];
                 }
         }
-        s[row] += dot;
+        //s[row] += dot;
 	__syncthreads();
 	y[row] = s[row];
 }
@@ -201,28 +240,41 @@ __global__ void vector_multiply_stride(const int num_rows,const int *ptr,const i
                 }*/
                 row_start = ptr[row];
                 row_end = ptr[row + 1];
-                #pragma unroll
                 for(i = row_start; i < row_end; i++){
                         dot+= data[i] * x[indices[i]];
                 }
       	}
-        /*if(row < num_rows){
+        y[row] += dot;
+}
+
+//TODO: Fix the prefetch method
+
+__global__ void vector_multiply_stride_prefetch(const int num_rows,const int *ptr,const int *indices,const float *data,const float *x, float* y){
+        int row;// = blockIdx.x * blockDim.x + threadIdx.x;
+        //printf("In Kernel:\n",row);
+        int i;
+        int row_start, row_end;
+        float dot;
+	#pragma unroll
+        for (row = blockIdx.x * blockDim.x + threadIdx.x; row < num_rows; row += blockDim.x * gridDim.x)
+        {
                 dot = 0;
-                printf("Kernel Matrix\n");
+		//__prefetch_global_l1(row_start);
+                /*printf("Kernel Matrix\n");
                 for(int l = 0; l<num_rows;l++){
                         if(l+1 <= num_rows){
                                 for(int k = ptr[l]; k < ptr[l+1];k++){
                                         printf("%d %d %.10f\n",l+1,indices[k]+1,data[k]);
                                 }
                         }
-                }
+                }*/
                 row_start = ptr[row];
                 row_end = ptr[row + 1];
                 #pragma unroll
                 for(i = row_start; i < row_end; i++){
                         dot+= data[i] * x[indices[i]];
                 }
-        }*/
+        }
         y[row] += dot;
 }
 
@@ -245,7 +297,7 @@ int main (int argc, char* argv[]){
 		int* row_ptr;
 		int* col_ind;
 		float* values;
-		int r_count, v_count, i, k;
+		int r_count, v_count, i, k, j;
 		int num = atoi(argv[1]);
 		int repetitions = atoi(argv[2]);
 		int mode = atoi(argv[3]);
@@ -260,6 +312,7 @@ int main (int argc, char* argv[]){
 		read_matrix(&row_ptr, &col_ind, &values, fname, &r_count, &v_count);
 		float* x =(float*) malloc(r_count* sizeof(float));
 		float* y =(float*) calloc(r_count, sizeof(float));
+		float* cy =(float*) calloc(r_count, sizeof(float));
 		for(i = 0; i<r_count;i++){
 		    	x[i]= (float)num;
 		}
@@ -281,6 +334,34 @@ int main (int argc, char* argv[]){
 		    	}
 
 	  	}
+		//TODO: Timer for CPU run
+		//*************************************************************
+                //RUNNING ON CPU
+                //*************************************************************
+		/*struct timeval cstart, cend;
+    		long mtime, secs, usecs;*/ 
+		float cdot;	
+		//gettimeofday(&cstart, NULL);
+		for(k = 0; k<repetitions; k++){
+			//cpu_multiply(r_count, row_ptr, col_ind, values, x, y);
+        		for (i=0; i<r_count; i++){
+                		cdot = 0;
+                		for(j = row_ptr[i]; j < row_ptr[i+1]; j++){
+                        		cdot+= values[j] * x[col_ind[j]];
+                		}
+                	cy[i] += cdot;
+        		}
+		}
+		/*gettimeofday(&cend, NULL);
+		secs  = cend.tv_sec  - cstart.tv_sec;
+    		usecs = cend.tv_usec - cstart.tv_usec;
+    		mtime = ((secs) * 1000 + usecs/1000.0) + 0.5;
+    		printf("Elapsed time: %ld millisecs\n", mtime);*/
+
+		//*************************************************************
+                //COMPLETED RUNNING ON CPU
+                //*************************************************************
+
 		//*************************************************************
 		//SETTING UP GPU RUN
 		//*************************************************************
@@ -288,10 +369,14 @@ int main (int argc, char* argv[]){
 		int *d_row_ptr, *d_col_ind;
 		float *d_values, *d_x, *d_y;
 
-		cudaEvent_t start,stop;
+		cudaEvent_t start,stop,st,sp;
                 cudaEventCreate(&start);
                 cudaEventCreate(&stop);
+		cudaEventCreate(&st);
+                cudaEventCreate(&sp);
 		float milliseconds = 0;
+		//float timing = 0;
+		//int bytes = r_count * sizeof(float);
 
 		switch(opti){
 			case 1:
@@ -312,7 +397,7 @@ int main (int argc, char* argv[]){
 
 				cudaEventRecord(start);
                	 		for(k = 0; k<repetitions; k++){
-                        		printf("Calling Kernel\n");
+                        		//printf("Calling Kernel\n");
                         		cudaMemcpy(d_x, x, r_count*sizeof(float), cudaMemcpyHostToDevice);
                         		cudaMemcpy(d_y, y, r_count*sizeof(float), cudaMemcpyHostToDevice);
 
@@ -355,7 +440,7 @@ int main (int argc, char* argv[]){
 
                                 cudaEventRecord(start);
                                 for(k = 0; k<repetitions; k++){
-                                        printf("Calling Kernel\n");
+                                        //printf("Calling Kernel\n");
                                         cudaMemcpy(d_x, x, r_count*sizeof(float), cudaMemcpyHostToDevice);
                                         cudaMemcpy(d_y, y, r_count*sizeof(float), cudaMemcpyHostToDevice);
 
@@ -397,7 +482,7 @@ int main (int argc, char* argv[]){
 
                                 cudaEventRecord(start);
                                 for(k = 0; k<repetitions; k++){
-                                        printf("Calling Kernel\n");
+                                        //printf("Calling Kernel\n");
                                         cudaMemcpy(d_x, x, r_count*sizeof(float), cudaMemcpyHostToDevice);
                                         cudaMemcpy(d_y, y, r_count*sizeof(float), cudaMemcpyHostToDevice);
 
@@ -425,6 +510,8 @@ int main (int argc, char* argv[]){
 				//*************************************************************
                                 //RUNNING KERNEL WITH DATA TRANSFER OPTIMIZATIONS
                                 //*************************************************************
+				
+				//int bytes = r_count * sizeof(float);
 
                           	cudaMallocHost(&d_row_ptr, r_count*sizeof(int));
                 		cudaMallocHost(&d_col_ind, v_count*sizeof(int));
@@ -433,13 +520,21 @@ int main (int argc, char* argv[]){
                 		cudaMallocHost(&d_y, r_count*sizeof(float));   
                             
                                 cudaMemcpy(d_row_ptr, row_ptr, r_count*sizeof(int), cudaMemcpyHostToDevice);
+
                                 cudaMemcpy(d_col_ind, col_ind, v_count*sizeof(int), cudaMemcpyHostToDevice);
                                 cudaMemcpy(d_values, values, v_count*sizeof(int), cudaMemcpyHostToDevice);
 
                                 cudaEventRecord(start);
                                 for(k = 0; k<repetitions; k++){
-                                        printf("Calling Kernel\n");
+                                        //printf("Calling Kernel\n");
+					//cudaEventRecord(st);
                                         cudaMemcpy(d_x, x, r_count*sizeof(float), cudaMemcpyHostToDevice);
+					//cudaEventRecord(sp);
+					//cudaEventSynchronize(sp);
+					//cudaEventElapsedTime(&timing, st, sp);
+					
+					//printf("Host to Device bandwidth (GB/s): %f\n", bytes * 1e-6 / timing);
+
                                         cudaMemcpy(d_y, y, r_count*sizeof(float), cudaMemcpyHostToDevice);
 
 
@@ -449,7 +544,13 @@ int main (int argc, char* argv[]){
                                         //blocknum = 1.0;
                                         //printf("blocknum: %f\n",blocknum);
                                         vector_multiply_unopti<<<blocknum, blocksize>>>(r_count, d_row_ptr, d_col_ind, d_values, d_x, d_y);
+					//cudaEventRecord(st);
                                         cudaMemcpy(y, d_y, r_count*sizeof(float), cudaMemcpyDeviceToHost);
+					//cudaEventRecord(sp);
+					//cudaEventSynchronize(sp);
+					//cudaEventElapsedTime(&timing, st, sp);
+
+                                        //printf("Device to Host bandwidth (GB/s): %f\n", bytes * 1e-6 / timing);
                                         for(i = 0; i<r_count;i++){
                                                 x[i] = y[i];
                                                 y[i]= 0.0;
@@ -461,6 +562,9 @@ int main (int argc, char* argv[]){
 
                                 milliseconds = 0;
                                 cudaEventElapsedTime(&milliseconds, start, stop);
+				
+				cudaEventDestroy(st);
+                		cudaEventDestroy(sp);
 
 				break;
 			case 5:
@@ -481,7 +585,7 @@ int main (int argc, char* argv[]){
 
                                 cudaEventRecord(start);
                                 for(k = 0; k<repetitions; k++){
-                                        printf("Calling Kernel\n");
+                                        //printf("Calling Kernel\n");
                                         cudaMemcpy(d_x, x, r_count*sizeof(float), cudaMemcpyHostToDevice);
                                         cudaMemcpy(d_y, y, r_count*sizeof(float), cudaMemcpyHostToDevice);
 
@@ -491,7 +595,7 @@ int main (int argc, char* argv[]){
                                         //printf("blocknum: %f\n",blocknum);
                                         //blocknum = 1.0;
                                         //printf("blocknum: %f\n",blocknum);
-                                        vector_multiply_unopti<<<blocknum, blocksize>>>(r_count, d_row_ptr, d_col_ind, d_values, d_x, d_y);
+                                        vector_multiply_unroll<<<blocknum, blocksize>>>(r_count, d_row_ptr, d_col_ind, d_values, d_x, d_y);
                                         cudaMemcpy(y, d_y, r_count*sizeof(float), cudaMemcpyDeviceToHost);
                                         for(i = 0; i<r_count;i++){
                                                 x[i] = y[i];
@@ -524,7 +628,7 @@ int main (int argc, char* argv[]){
 
                                 cudaEventRecord(start);
                                 for(k = 0; k<repetitions; k++){
-                                        printf("Calling Kernel\n");
+                                        //printf("Calling Kernel\n");
                                         cudaMemcpy(d_x, x, r_count*sizeof(float), cudaMemcpyHostToDevice);
                                         cudaMemcpy(d_y, y, r_count*sizeof(float), cudaMemcpyHostToDevice);
 
@@ -533,7 +637,7 @@ int main (int argc, char* argv[]){
                                         float blocknum = ceil(r_count/blocksize);
                                         //printf("blocknum: %f\n",blocknum);
                                         //blocknum = 1.0;
-                                        printf("blocknum: %f\n",blocknum);
+                                        //printf("blocknum: %f\n",blocknum);
                                         vector_multiply_shared<<<blocknum, blocksize>>>(r_count, d_row_ptr, d_col_ind, d_values, d_x, d_y);
                                         cudaMemcpy(y, d_y, r_count*sizeof(float), cudaMemcpyDeviceToHost);
                                         for(i = 0; i<r_count;i++){
@@ -566,7 +670,7 @@ int main (int argc, char* argv[]){
 
                                 cudaEventRecord(start);
                                 for(k = 0; k<repetitions; k++){
-                                        printf("Calling Kernel\n");
+                                        //printf("Calling Kernel\n");
                                         cudaMemcpy(d_x, x, r_count*sizeof(float), cudaMemcpyHostToDevice);
                                         cudaMemcpy(d_y, y, r_count*sizeof(float), cudaMemcpyHostToDevice);
 
@@ -659,6 +763,7 @@ int main (int argc, char* argv[]){
 
 		}
 		fprintf(stdout,"time = %f\n", milliseconds);
+		//fprintf(stdout,"CPU time = %f\n",cpu_time_used);
 		
 		//*************************************************************
                 //FREE MEMORY
@@ -668,6 +773,7 @@ int main (int argc, char* argv[]){
 		cudaFree(d_values);
 		cudaFree(d_x);
 		cudaFree(d_y);
+		cudaEventDestroy(start);
+        	cudaEventDestroy(stop);
 	}
 }
-
